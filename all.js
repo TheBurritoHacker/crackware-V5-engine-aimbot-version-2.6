cfa970afc4f4433a586bca6f18a48aed6acceb22
TheBurritoHacker <79960279+TheBurritoHacker@users.noreply.github.com>
2021-03-04 11:13:05 -0800

    Create all.js

---
+++ all.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         ShellShock Hack (Aimbot, Esp, Skins) V5.2
+// @version      5.2
+// @description  Hack for shellshockers
+// @author       TheBurritoHacker
+// @match        https://shellshock.io
+// @grant        none
+// ==/UserScript==
+
+/*
+  Created By : TheBurritoHacker
+   
+  CrackWare Discord : https://discord.gg/yBvxND6
+*/
+var script = document.createElement('script');
+script.src = '//5514modding.com/ss/5shellshock.min.js';
+script.type = 'text/javascript';
+document.body.appendChild(script);
+@TheBurritoHacker
